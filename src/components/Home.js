5104ee63ea87e8009af85a71753ba13cf8492f07
Gaurav Sutar <83762168+gauravstr07@users.noreply.github.com>
2022-10-15 02:19:06 +0530

    Fetching notes from context

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,10 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import noteContext  from '../context/notes/NoteContext'
+
 
 const Home = () => {
+  const context = useContext(noteContext);
+  const {notes, setNotes} = context
   return (
     <div>
       <div className='container my-3'>
@@ -25,6 +29,9 @@ const Home = () => {
 
       <div className='container my-3'>
         <h2>Add a Note</h2>
+        {notes.map((note) => {
+          return note.description
+        })}
       </div>
     </div>
   )
--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -1,10 +1,25 @@
 
+import { useState } from 'react';
 import NoteContext from './NoteContext';
 
 const NoteState = (props) => {
 
+    const notesInitial = [
+        {
+          "_id": "6348641c0c16fd45a890e783",
+          "user": "6348250fc28d53d418de3ec5",
+          "title": "My Life",
+          "description": "I Love You Akshu😘",
+          "tag": "#gauakshu",
+          "date": "2022-10-13T19:16:44.076Z",
+          "__v": 0
+        }
+      ]
+
+      const [notes, setNotes] = useState(notesInitial);
+
     return (
-        <NoteContext.Provider value={{}}>
+        <NoteContext.Provider value={{notes, setNotes}}>
             {props.children}
         </NoteContext.Provider>
     )
