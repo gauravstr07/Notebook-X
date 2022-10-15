fe8ecc67fb53f03c6570e857ac1c9d1f4b59f65f
Gaurav Sutar <83762168+gauravstr07@users.noreply.github.com>
2022-10-15 13:03:15 +0530

    Adding AddNote Component

--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -4,7 +4,7 @@ import NoteContext from "./NoteContext";
 const NoteState = (props) => {
   const notesInitial = [
     {
-      _id: "6348641c0c16fd45a890e783",
+      _id: "6348641c0c16fd45a8902e783",
       user: "6348250fc28d53d418de3ec5",
       title: "My Life",
       description: "I Love You Akshu😘",
@@ -13,7 +13,7 @@ const NoteState = (props) => {
       __v: 0,
     },
     {
-      _id: "6348641c0c16fd45a890e783",
+      _id: "6348641c0c16fd45a89047e783",
       user: "6348250fc28d53d418de3ec5",
       title: "My Family",
       description: "I Love My Family",
@@ -22,7 +22,7 @@ const NoteState = (props) => {
       __v: 0,
     },
     {
-        _id: "6348641c0c16fd45a890e783",
+        _id: "6348641c0c16fd4895a890e783",
         user: "6348250fc28d53d418de3ec5",
         title: "My Family",
         description: "I Love My Family",
@@ -31,7 +31,7 @@ const NoteState = (props) => {
         __v: 0,
       },
       {
-        _id: "6348641c0c16fd45a890e783",
+        _id: "6348641c0c16fd45a890e21783",
         user: "6348250fc28d53d418de3ec5",
         title: "My Family",
         description: "I Love My Family",
@@ -40,7 +40,7 @@ const NoteState = (props) => {
         __v: 0,
       },
       {
-        _id: "6348641c0c16fd45a890e783",
+        _id: "6348641c0c16fd4905a890e783",
         user: "6348250fc28d53d418de3ec5",
         title: "My Family",
         description: "I Love My Family",
@@ -50,10 +50,36 @@ const NoteState = (props) => {
       },
   ];
 
-  const [notes, setNotes] = useState(notesInitial);
+  let [notes, setNotes] = useState(notesInitial);
+
+  // Add a Note
+  let addNote = (title, description, tag) => {
+    console.log('Adding a new Note')
+   let note = {
+     " _id": "6348641c0c16fd45a211890e783",
+      "user": "6348250fc28d53d418de3ec5",
+      "title": title,
+      "description": description,
+      "tag": tag,
+      "date": "2022-10-13T19:16:44.076Z",
+      "__v": 0,
+    };
+    setNotes(notes.concat(note));
+  }
+
+
+  // Delete a Note
+  const deleteNote = (id) => {
+    
+  }
+
+  //Edit a Note
+  const editNote = () => {
+
+  }
 
   return (
-    <NoteContext.Provider value={{ notes, setNotes }}>
+    <NoteContext.Provider value={{ notes, addNote, deleteNote, editNote }}>
       {props.children}
     </NoteContext.Provider>
   );
